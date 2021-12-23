476cc9438d88ccaf5527a906babe2b97ac0f9483
Anjan Prasad <anjanprasad112@gmail.com>
2021-12-23 08:58:46 +0530

    simply

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,4 @@
-
 import './App.css';
-
 function App() {
   // let myvariable="Anjan Prasad"
   return (
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import Navbar from './Navbar'
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>,
   document.getElementById('root')
 );
